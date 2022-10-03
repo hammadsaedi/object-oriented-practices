93059afd1ff228d7f7f0f58fb25c8f84779875ca
Hammad Saaedi <faoqzuhair@gmail.com>
2022-10-03 22:43:35 +0500

    Single Object

---
+++ Playground/Main.java
@@ -0,0 +1,18 @@
+package Playground;
+
+public class Main {
+    public static void main(String[] args) {
+        // The constructor Single() is not visibleJava(134217859)
+        // Single single = new Single();
+
+        Single single = Single.getInstance();
+        System.out.println(single.getDob());
+
+        try {
+            Single second = Single.getInstance();
+            System.out.println(second);
+        } catch (RuntimeException rte) {
+            System.out.println(rte);
+        }
+    }
+}
---
+++ Playground/Single.java
@@ -0,0 +1,63 @@
+package Playground;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Single {
+    /**
+     * Instance of class
+    */
+    private static Single object;
+    
+    /**
+     * Instantiate Class:
+     * Set to private; Unaccessible outside of class
+     */
+    private Single () {
+        this.dob =  LocalDate.now().getDayOfMonth();
+    }
+
+    /**
+     * @return Instance of Class
+     * @throws RuntimeException Cannot Instantiate Multiple Objects
+     * @apiNote Static Method
+     */
+    public static Single getInstance(){
+        if (Objects.nonNull(object)) throw new RuntimeException("Cannot Instantiate Multiple Objects.");
+        return object = new Single();
+    }
+
+    /**
+     * State Variable
+    */
+    private String name;
+    private int dob;
+
+    /**
+     * @return Name of Object
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set name
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get Day of Instantiation
+     * @return
+     */
+    public int getDob() {
+        return dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Single [name=" + name + ", dob=" + dob + "]";
+    }    
+}
