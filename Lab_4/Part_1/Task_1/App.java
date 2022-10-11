f7579cd69d0538b4878b36aa65f4321a37eb103e
Hammad Saaedi <faoqzuhair@gmail.com>
2022-10-11 22:53:16 +0500

    Lab 4

---
+++ Lab_4/Part_1/Task_1/App.java
@@ -0,0 +1,35 @@
+package Lab_4.Part_1.Task_1;
+
+public class App {
+    public static void main(String[] args) {
+        Point[] points = new Point[10];
+        
+        for (int i = 0; i < points.length; i++) {
+            System.out.println(points[i]);    
+        }
+
+        for (int i = 0; i < 5; i++) {
+            points[i] = new Point();
+        }
+
+        for (int i = 5; i < 10; i++) {
+            points[i] = new Point(i, 2*i);
+        }
+
+        for (int i = 0; i < points.length; i++) {
+            System.out.println(points[i]);    
+        }
+
+        for (int i = 0; i < 5; i++) {
+            points[i].setY(19);
+        }
+
+        for (int i = 5; i < 10; i++) {
+            points[i].move(3, 4);
+        }
+
+        for (int i = 0; i < points.length; i++) {
+            System.out.println(points[i]);    
+        }
+    }
+}
---
+++ Lab_4/Part_2/Task_1/Rectangle.java
@@ -0,0 +1,78 @@
+package Lab_4.Part_2.Task_1;
+
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(Rectangle obj) {
+        this(obj.length, obj.width);
+    }
+
+    public Rectangle() {
+        this(0, 0);
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+    
+    public void setLength(double length) {
+        if (length >= 0)
+            this.length = length;
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+    
+    public void setWidth(double width) {
+        if (width >= 0)
+            this.width = width;
+    }
+
+    public double calculateArea(){
+        return length * width; 
+    }
+
+    public boolean checkSquare(){
+        if (Double.doubleToLongBits(length) == 0 && Double.doubleToLongBits(width) == 0) 
+            return true;
+        return false;
+    }
+
+    public int compare(Rectangle obj){
+        if (this.calculateArea() > obj.calculateArea())
+            return 1;
+        if (this.calculateArea() < obj.calculateArea())
+            return -1;
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) 
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Rectangle other = (Rectangle) obj;
+        if (Double.doubleToLongBits(this.length) != Double.doubleToLongBits(other.width))
+            return false;
+        if (Double.doubleToLongBits(this.length) != Double.doubleToLongBits(other.width))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return "Rectangle [Length: " + length + ", " + "Width: " + width + "]";
+    }
+    
+}
